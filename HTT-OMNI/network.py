7616d106bf0bce197a796e46e2945024102b58d0
Michelle Kennedy <mak4515@users.noreply.github.com>
2022-03-18 09:40:56 -0400

    Update network.py

--- HTT-OMNI/network.py
+++ HTT-OMNI/network.py
@@ -5,17 +5,28 @@ import panel as pn
 from draggable_graph import DraggableGraph
 
 class Network(param.Parameterized):
-    # will contain a list of [nodes, edges, graph_opts, layout_algorithm, bundle_graph_edges] to be displayed in the network
-    network_data = hv.streams.Pipe(linked=False)
+    
+    node_data = param.DataFrame()
+    edge_data = param.DataFrame()
+    graph_opts_data = param.Parameter()
+    
+    # data pipes to push data to DynamicMaps
+    network_data = hv.streams.Pipe() #ultimately this will be a list of node_data, edge_data, graph_opts_data, layout, bundle_edge_graphs
+    enrichment_data = hv.streams.Pipe()
     
     # slider for # of nodes to show
-    slider = param.Integer()
+    max_nodes = param.Selector(objects = [10, 20, 50, 100, 200, 300, 400, 500], default=50)
+    
+    # graph layout algorithm
+    layout = param.Selector(objects = ['kamada_kawai', 'circular', 'spring'], default='kamada_kawai')
+    
+    # edge bundling
+    bundle_graph_edges = param.Selector(objects = ['Yes', 'No'], default='No')
     
     def __init__(self, 
                  nodes, 
                  edges, 
                  graph_opts = [], 
-                 layout_algorithm = 'circular', 
                  bundle_graph_edges = False, 
                  index_col = 'GeneID',
                  source_col = 'GENE_ID_A',
@@ -34,25 +45,35 @@ class Network(param.Parameterized):
         self.label_col = label_col
         
         self.network_pane = pn.pane.HoloViews(sizing_mode = 'stretch_both', linked_axes=False, min_height=0, min_width=0)
+        
         self.graph = DraggableGraph(
             index_col = self.index_col,
             source_col = self.source_col,
             target_col = self.target_col,
             label_col = self.label_col,
         )
-        self.network_data.update(data = [nodes, edges, graph_opts, layout_algorithm, bundle_graph_edges])
+        self.network_data.update(data = [nodes, edges, graph_opts, self.layout, bundle_graph_edges])
         
-        self.slider = nodes.shape[0]
-        self.param.slider.bounds = (1, nodes.shape[0])
         
-    @param.depends('network_data.data', watch=True)
+    @param.depends('network_data.data', watch=True) # this method is the money maker!
     def view(self):
         self.network_pane.object = self.graph.view(self.network_data.data)
         
-    @param.depends('slider', watch=True)
+    @param.depends('max_nodes', watch=True)
     def update_nodes_edges(self):
-        new_nodes = self.nodes.iloc[:self.slider,:]
+        new_nodes = self.nodes.iloc[:self.max_nodes,:]
         new_edges = self.edges[self.edges[self.source_col].isin(new_nodes[self.index_col])&self.edges[self.target_col].isin(new_nodes[self.index_col])].copy()
         
         new_data = [new_nodes, new_edges]+self.network_data.data[2:]
         self.network_data.update(data=new_data)
+        
+    @param.depends('layout', watch=True)
+    def update_layout(self):
+        new_data = self.network_data.data[:3]+[self.layout, self.network_data.data[-1]]
+        self.network_data.update(data=new_data)
+        
+    @param.depends('bundle_graph_edges', watch=True)
+    def update_bundling(self):
+        new_data = self.network_data.data[:4]+[{'Yes': True, 'No': False}[self.bundle_graph_edges]]
+        self.network_data.update(data=new_data)
+        
