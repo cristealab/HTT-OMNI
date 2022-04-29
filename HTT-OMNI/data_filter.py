7fb1cb0b2e86e623c4fca2e238bf5a89bd649e11
mak4515 <49048165+mak4515@users.noreply.github.com>
2022-04-29 16:27:47 -0400

    Update data_filter.py

--- HTT-OMNI/data_filter.py
+++ HTT-OMNI/data_filter.py
@@ -64,7 +64,7 @@ class DataFilter(param.Parameterized):
         self.target_col = target_col
         self.edge_score_col = edge_score_col
         
-        self.STRINGdb_edgefile = None
+        self.STRINGdb_edgefile = pn.state.cache['STRINGdb_edgefile']
         self.user_data = None
         
         if filter_aliases is None:
@@ -354,10 +354,6 @@ class DataFilter(param.Parameterized):
 
             self.nodes = new_nodes
             self.annotate()
-            
-            # get associated edges
-            if self.STRINGdb_edgefile is None:
-                self.STRINGdb_edgefile = pd.read_csv(r'.\assets\data\STRINGdb_edgefile.csv')
                 
             gids = np.unique(self.nodes[self.index_col])
             in_source = self.STRINGdb_edgefile[self.source_col].isin(gids)
